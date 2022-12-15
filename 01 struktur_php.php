8e468bae8104ef4e7116d7577e23bba7242f298c
fathyafathazz <fathyafathazzra@gmail.com>
2022-12-15 11:03:50 +0700

    Prakikum Modul PHP

---
+++ 01 struktur_php.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Struktur Penulisan PHP</title>
+    </head>
+    <body>
+        <h2>Struktur Penulisan PHP</h2>
+        <?php
+        echo("Selamat datang di kawasan PHP!"); ?> <br>
+        <!-- versi print -->
+        <?php
+        print("Selamat datang di kawasan PHP!"); ?>
+    </body>
+</html>
